3345f000471d110831bef300ebcd382687271d40
Veli Tasali <veli.tasali@gmail.com>
2019-10-10 20:16:44 +0300

    Remove build files

--- cmake-build-debug/CMakeFiles/links_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/links_autogen"
-  "links_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/links_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
